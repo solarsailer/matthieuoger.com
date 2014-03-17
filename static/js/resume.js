57f45ec21ae8698689c6899929a2ecbfaffb30d0
Matthieu Oger <moger@pixelnest.io>
2014-03-17 23:35:41 +0100

    Resume.js: fix prop.

--- static/js/resume.js
+++ static/js/resume.js
@@ -1,4 +1,4 @@
 window.onload = function () {
   var age = new Date().getFullYear()
-  document.getElementById('age').innerText = (age - 1988) + ' ans'
+  document.getElementById('age').innerHTML = (age - 1988) + ' ans'
 }
