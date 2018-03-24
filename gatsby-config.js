87c2ad8d4e37d89c85e2a48b1c6da75120b379a5
Matthieu Oger <moger@pixelnest.io>
2018-03-24 12:29:34 +0100

    Add gatsby plugins

---
+++ gatsby-config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  plugins: [
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        path: `${__dirname}/content/posts`,
+        name: 'posts'
+      }
+    },
+    `gatsby-transformer-remark`
+  ]
+}
