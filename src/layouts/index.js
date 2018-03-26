695829dd8b16aea01f2df23be203fc591d257d22
Matthieu Oger <moger@pixelnest.io>
2018-03-26 18:36:14 +0200

    Add meta tags

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -4,22 +4,20 @@ import {Helmet} from 'react-helmet'
 import Header from '../components/Header'
 import Footer from '../components/Footer'
 
+import Card from './card.jpg'
+
 // -------------------------------------------------------------
 // Component.
 // -------------------------------------------------------------
 
 export default ({data, children}) => {
-  const {site: {siteMetadata}} = data
+  const {site: {siteMetadata: metadata}} = data
+  const {handles} = metadata
+
+  const fullUrl = `http://${metadata.url}/`
 
   return (
     <div className="page" id="page">
-      <Helmet>
-        <title>{siteMetadata.title}</title>
-        <meta name="author" content={siteMetadata.author} />
-        <meta name="description" content={siteMetadata.description} />
-        <meta name="keywords" content={siteMetadata.keywords.join(', ')} />
-      </Helmet>
-
       <Header />
 
       <main className="page-content" id="page_content" role="main">
@@ -27,6 +25,45 @@ export default ({data, children}) => {
       </main>
 
       <Footer />
+
+      <Helmet>
+        <title>{metadata.title}</title>
+
+        {/*
+        <link rel="shortcut icon" href={Favicon}>
+        <link rel="alternate" type="application/atom+xml" title={metadata.title} href="FEED.ATOM">
+        <link rel="alternate" type="application/json" title={metadata.title} href="FEED.JSON">
+        */}
+
+        <meta name="author" content={metadata.author} />
+        <meta name="description" content={metadata.description} />
+        <meta name="keywords" content={metadata.keywords.join(', ')} />
+
+        {/* Twitter */}
+        <meta name="twitter:title" content={metadata.title} />
+        <meta name="twitter:description" content={metadata.description} />
+        <meta name="twitter:card" content="summary" />
+        <meta name="twitter:site" content={handles.twitter} />
+        <meta name="twitter:creator" content={handles.twitter} />
+        <meta name="twitter:url" content={fullUrl} />
+        <meta name="twitter:image" content={Card} />
+        <meta
+          name="twitter:image:alt"
+          content={`${metadata.title} — ${metadata.description}`}
+        />
+
+        {/* OpenGraph */}
+        <meta property="og:locale" content="en" />
+        <meta property="og:title" content={metadata.title} />
+        <meta property="og:site_name" content={metadata.title} />
+        <meta property="og:description" content={metadata.description} />
+        <meta property="og:url" content={fullUrl} />
+        <meta property="og:image" content={Card} />
+        <meta property="og:image:height" content="1920" />
+        <meta property="og:image:width" content="1080" />
+        <meta property="og:image:type" content="image/jpg" />
+        <meta property="og:type" content="website" />
+      </Helmet>
     </div>
   )
 }
@@ -43,6 +80,14 @@ export const LAYOUT_QUERY = graphql`
         author
         description
         keywords
+        url
+        handles {
+          github
+          twitter
+          dribbble
+          unsplash
+          instagram
+        }
       }
     }
   }
