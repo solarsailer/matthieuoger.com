27ca79de115155c3ea113e8db0bd35ea9a4bbb7b
Matthieu Oger <moger@pixelnest.io>
2018-03-26 17:55:19 +0200

    Add metadata to config

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,7 +3,30 @@
 // -------------------------------------------------------------
 
 exports.siteMetadata = {
-  title: `Solar Sailer`
+  title: 'Solar Sailer',
+  author: 'Matthieu Oger',
+  description: 'A collection of thoughts and opinions by Matthieu Oger',
+  keywords: [
+    'solar',
+    'sailer',
+    'matthieu',
+    'oger',
+    'technology',
+    'design',
+    'development',
+    'dev',
+    'photography',
+    'pixelnest'
+  ],
+  handles: {
+    github: 'solarsailer',
+    twitter: 'solarsailer',
+    dribbble: 'solarsailer',
+    unsplash: 'solarsailer',
+    instagram: 'matthieuoger'
+  },
+  googleAnalyticsCode: 'UA-41853457-1',
+  url: 'solarsailer.net'
 }
 
 // -------------------------------------------------------------
