b7a49043908c13823a9da5b133a19599df3ecc55
Matthieu Oger <moger@pixelnest.io>
2018-03-26 10:54:14 +0200

    Add posts list in index

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,12 +1,49 @@
 import React from 'react'
 
-export default () => (
-  <ul>
-    <li>
-      <a href="/2016/04/01/">Cats, taverns and cleaning systems</a>
+// -------------------------------------------------------------
+// Module.
+// -------------------------------------------------------------
+
+function mapNode({id, excerpt, frontmatter}) {
+  return (
+    <li key={id}>
+      <h2>
+        {frontmatter.title} <span>{frontmatter.date}</span>
+      </h2>
+      <div>
+        <p>{excerpt}</p>
+      </div>
     </li>
-    <li>
-      <a href="/about/">About</a>
-    </li>
-  </ul>
-)
+  )
+}
+
+export default ({data}) => {
+  const edges = data.allMarkdownRemark.edges
+
+  return (
+    <main role="main">
+      <ul>{edges.map(({node}) => mapNode(node))}</ul>
+    </main>
+  )
+}
+
+// -------------------------------------------------------------
+// Query.
+// -------------------------------------------------------------
+
+export const POSTS_LIST_QUERY = graphql`
+  query PostsListQuery {
+    allMarkdownRemark(sort: {order: DESC, fields: [frontmatter___date]}) {
+      edges {
+        node {
+          id
+          frontmatter {
+            title
+            date(formatString: "DD MMMM YYYY")
+          }
+          excerpt
+        }
+      }
+    }
+  }
+`
