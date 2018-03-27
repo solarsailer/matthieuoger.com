39c08ffb61a0549d2e04b7f320dcc9b95585c28b
Matthieu Oger <moger@pixelnest.io>
2018-03-27 20:08:20 +0200

    Refactor Meta in component

---
+++ src/components/Meta.js
@@ -0,0 +1,70 @@
+import React, {Fragment} from 'react'
+import {Helmet} from 'react-helmet'
+
+import Card from '../images/card.jpg'
+
+import '../styles/globals/manifest'
+
+// -------------------------------------------------------------
+// Component.
+// -------------------------------------------------------------
+
+export default ({site}) => {
+  const {siteMetadata: metadata} = site
+  const {handles} = metadata
+
+  const googleFonts = metadata.googleFonts.map(x => (
+    <link
+      key={x}
+      rel="stylesheet"
+      href={'https://fonts.googleapis.com/css?family=' + x}
+    />
+  ))
+
+  const fullUrl = `http://${metadata.url}/`
+
+  return (
+    <Fragment>
+      <Helmet>
+        {googleFonts}
+
+        <title>{metadata.title}</title>
+
+        {/*
+        <link rel="shortcut icon" href={Favicon}>
+        <link rel="alternate" type="application/atom+xml" title={metadata.title} href="feed.xml">
+        <link rel="alternate" type="application/json" title={metadata.title} href="feed.json">
+        */}
+
+        <meta name="author" content={metadata.author} />
+        <meta name="description" content={metadata.description} />
+        <meta name="keywords" content={metadata.keywords.join(', ')} />
+
+        {/* Twitter */}
+        <meta name="twitter:title" content={metadata.title} />
+        <meta name="twitter:description" content={metadata.description} />
+        <meta name="twitter:card" content="summary" />
+        <meta name="twitter:site" content={handles.twitter} />
+        <meta name="twitter:creator" content={handles.twitter} />
+        <meta name="twitter:url" content={fullUrl} />
+        <meta name="twitter:image" content={Card} />
+        <meta
+          name="twitter:image:alt"
+          content={`${metadata.title} — ${metadata.description}`}
+        />
+
+        {/* OpenGraph */}
+        <meta property="og:locale" content="en" />
+        <meta property="og:title" content={metadata.title} />
+        <meta property="og:site_name" content={metadata.title} />
+        <meta property="og:description" content={metadata.description} />
+        <meta property="og:url" content={fullUrl} />
+        <meta property="og:image" content={Card} />
+        <meta property="og:image:height" content="1920" />
+        <meta property="og:image:width" content="1080" />
+        <meta property="og:image:type" content="image/jpg" />
+        <meta property="og:type" content="website" />
+      </Helmet>
+    </Fragment>
+  )
+}
--- src/layouts/index.js
+++ src/layouts/index.js
@@ -2,37 +2,17 @@ import React from 'react'
 import styled from 'styled-components'
 import {Helmet} from 'react-helmet'
 
+import Meta from '../components/Meta'
 import Header from '../components/Header'
 import Footer from '../components/Footer'
-
-import Card from '../images/card.jpg'
-
-import '../styles/globals/manifest'
 
 // -------------------------------------------------------------
 // Component.
 // -------------------------------------------------------------
 
-const Page = styled.div`
-  padding: 2rem;
-`
-
 export default ({data, children}) => {
-  const {site: {siteMetadata: metadata}} = data
-  const {handles} = metadata
-
-  const googleFonts = metadata.googleFonts.map(x => (
-    <link
-      key={x}
-      rel="stylesheet"
-      href={'https://fonts.googleapis.com/css?family=' + x}
-    />
-  ))
-
-  const fullUrl = `http://${metadata.url}/`
-
   return (
-    <Page className="page" id="page">
+    <div className="page" id="page">
       <Header />
 
       <main className="page-content" id="page_content" role="main">
@@ -41,47 +21,8 @@ export default ({data, children}) => {
 
       <Footer />
 
-      <Helmet>
-        {googleFonts}
-
-        <title>{metadata.title}</title>
-
-        {/*
-        <link rel="shortcut icon" href={Favicon}>
-        <link rel="alternate" type="application/atom+xml" title={metadata.title} href="feed.xml">
-        <link rel="alternate" type="application/json" title={metadata.title} href="feed.json">
-        */}
-
-        <meta name="author" content={metadata.author} />
-        <meta name="description" content={metadata.description} />
-        <meta name="keywords" content={metadata.keywords.join(', ')} />
-
-        {/* Twitter */}
-        <meta name="twitter:title" content={metadata.title} />
-        <meta name="twitter:description" content={metadata.description} />
-        <meta name="twitter:card" content="summary" />
-        <meta name="twitter:site" content={handles.twitter} />
-        <meta name="twitter:creator" content={handles.twitter} />
-        <meta name="twitter:url" content={fullUrl} />
-        <meta name="twitter:image" content={Card} />
-        <meta
-          name="twitter:image:alt"
-          content={`${metadata.title} — ${metadata.description}`}
-        />
-
-        {/* OpenGraph */}
-        <meta property="og:locale" content="en" />
-        <meta property="og:title" content={metadata.title} />
-        <meta property="og:site_name" content={metadata.title} />
-        <meta property="og:description" content={metadata.description} />
-        <meta property="og:url" content={fullUrl} />
-        <meta property="og:image" content={Card} />
-        <meta property="og:image:height" content="1920" />
-        <meta property="og:image:width" content="1080" />
-        <meta property="og:image:type" content="image/jpg" />
-        <meta property="og:type" content="website" />
-      </Helmet>
-    </Page>
+      <Meta site={data.site} />
+    </div>
   )
 }
 
