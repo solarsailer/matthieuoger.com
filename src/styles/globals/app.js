d2a2a1c455da2e9903105b587a16c5b0791d8226
Matthieu Oger <moger@pixelnest.io>
2018-03-27 20:16:22 +0200

    Change style of home layout

--- src/styles/globals/app.js
+++ src/styles/globals/app.js
@@ -4,4 +4,16 @@ import {css} from 'styled-components'
 // App Global Styles.
 // -------------------------------------------------------------
 
-export default css``
+export default css`
+  /**
+   * Change backgrounds for 'home' layout.
+   */
+
+  html[data-layout='home'] {
+    background-color: #1a1016;
+  }
+
+  html[data-layout='home'] body {
+    background-color: #1a1016;
+  }
+`
