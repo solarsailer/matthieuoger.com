1f94143c1f4091efacf8605702bb3091f82deee5
Matthieu Oger <moger@pixelnest.io>
2018-03-27 22:21:23 +0200

    Add default styles

--- src/styles/globals/app.js
+++ src/styles/globals/app.js
@@ -6,6 +6,31 @@ import {css} from 'styled-components'
 
 export default css`
   /**
+   * Reset.
+   */
+
+  h1,
+  h2,
+  h3,
+  h4,
+  h5,
+  h6,
+  p,
+  hr,
+  ul,
+  ol,
+  dl {
+    margin-top: initial;
+    margin-bottom: 2rem;
+  }
+
+  ol,
+  ul {
+    list-style: none;
+    padding-left: initial;
+  }
+
+  /**
    * Change backgrounds for 'home' layout.
    */
 
