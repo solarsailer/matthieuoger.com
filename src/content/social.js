1d38c24e84d7374f5b1c1aeb7ec610553cf65762
Matthieu Oger <moger@pixelnest.io>
2018-05-24 17:56:08 +0200

    Add social content object

---
+++ src/content/social.js
@@ -0,0 +1,40 @@
+// -------------------------------------------------------------
+// Content.
+// -------------------------------------------------------------
+
+export const social = {
+  twitter: {
+    name: 'Twitter',
+    color: '#4DA4FA',
+    domain: 'com',
+    url: 'https://twitter.com/@HANDLE/'
+  },
+  github: {
+    name: 'GitHub',
+    color: '#6CC644',
+    domain: 'com',
+    url: 'https://github.com/@HANDLE/'
+  },
+  dribbble: {
+    name: 'Dribbble',
+    color: '#EA4C89',
+    domain: 'com',
+    url: 'https://dribbble.com/@HANDLE/'
+  },
+  unsplash: {
+    name: 'Unsplash',
+    color: '#B3B3B3',
+    domain: 'com',
+    url: 'https://unsplash.com/@HANDLE/'
+  },
+  instagram: {
+    name: 'Instagram',
+    color: '#AB62EE',
+    domain: 'com',
+    url: 'https://instagram.com/@HANDLE/'
+  }
+}
+
+export function getSocial(name) {
+  return social[name]
+}
