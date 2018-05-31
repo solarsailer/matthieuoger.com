672765f457eaece804fe75e5d4a2300c42497cdf
Matthieu Oger <moger@pixelnest.io>
2018-05-31 16:48:33 +0200

    Finish footer

--- src/components/layouts/Footer.js
+++ src/components/layouts/Footer.js
@@ -3,24 +3,52 @@ import styled from 'styled-components'
 
 import SocialNavigation from '../CompactSocialNavigation'
 
+import {colors} from '../../styles/config'
+
 // -------------------------------------------------------------
 // Components.
 // -------------------------------------------------------------
 
 const Footer = styled.footer`
   color: #aaa;
-  text-align: center;
+  max-width: 500px;
+  margin-left: auto;
+  margin-right: auto;
 
-  font-size: 0.75em;
+  margin-top: 10rem;
+  margin-bottom: 3rem;
+  padding-top: 3rem;
+
+  border-top: 1px solid rgba(0, 0, 0, 0.1);
 
   ul {
     margin-bottom: 1rem;
   }
 `
 
+const Copyright = styled.p`
+  font-size: 0.75em;
+  text-align: center;
+  text-transform: uppercase;
+
+  a {
+    color: inherit;
+    transition: all 0.3s ease-out;
+    text-decoration: none;
+  }
+
+  a:hover {
+    color: ${colors.brand.main};
+  }
+`
+
 export default ({handles}) => (
   <Footer className="page-footer" id="page_footer">
     <SocialNavigation handles={handles} />
-    <p>© 2018 Matthieu Oger</p>
+    <Copyright>
+      <a href="/">
+        © 2018 <span>Matthieu Oger</span>
+      </a>
+    </Copyright>
   </Footer>
 )
