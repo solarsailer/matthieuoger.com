f35c74edccb4adc53fd82435558e0e7477bf48a8
Matthieu Oger <moger@pixelnest.io>
2018-06-01 12:03:18 +0200

    Improve header and make responsive

--- src/components/layouts/Header.js
+++ src/components/layouts/Header.js
@@ -1,7 +1,14 @@
 import React from 'react'
 import styled from 'styled-components'
+import Link from 'gatsby-link'
 
 import {siteNavigation} from '../../../content/config/site-navigation'
+
+// -------------------------------------------------------------
+// Constants.
+// -------------------------------------------------------------
+
+const BREAKPOINT = 700
 
 // -------------------------------------------------------------
 // Components.
@@ -11,27 +18,44 @@ const Header = styled.header`
   display: flex;
   align-items: center;
 
-  max-width: 750px;
+  max-width: 1000px;
   margin-left: auto;
   margin-right: auto;
+  margin-bottom: 10rem;
   padding: 4rem;
 
   line-height: normal;
+
+  @media (max-width: ${BREAKPOINT}px) {
+    flex-direction: column;
+    margin-bottom: 5rem;
+  }
 `
 
 const TitleBlock = styled.div`
-  margin-right: 5rem;
+  margin-right: auto;
 
   h1 {
     margin: 0;
     font-size: 1.8em;
     font-weight: 300;
     text-transform: uppercase;
+
+    a {
+      text-decoration: none;
+    }
   }
 
   aside {
     font-weight: bold;
     text-transform: uppercase;
+  }
+
+  @media (max-width: ${BREAKPOINT}px) {
+    margin-right: 0;
+    margin-bottom: 4rem;
+
+    text-align: center;
   }
 `
 
@@ -43,23 +67,33 @@ const List = styled.ul`
   display: flex;
   flex: 1;
 
-  justify-content: space-around;
+  justify-content: flex-end;
   align-items: center;
 
   margin: 0;
+
+  li + li {
+    margin-left: 2rem;
+  }
+
+  @media (max-width: ${BREAKPOINT}px) {
+    justify-content: center;
+  }
 `
 
 export default () => (
   <Header className="page-header" id="page_header">
     <TitleBlock>
-      <h1>Solar Sailer</h1>
+      <h1>
+        <Link to="/">Solar Sailer</Link>
+      </h1>
       <aside>Matthieu Oger</aside>
     </TitleBlock>
     <NavBlock>
       <List>
         {siteNavigation.map(x => (
           <li key={x.url}>
-            <a href={x.url}>{x.name}</a>
+            <Link to={x.url}>{x.name}</Link>
           </li>
         ))}
       </List>
