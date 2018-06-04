a53943dd5232d9cfb9eaa0546dac775d9ce0e466
Matthieu Oger <moger@pixelnest.io>
2018-06-05 01:14:15 +0200

    Generate link tags for feeds

---
+++ plugins/gatsby-plugin-feeds/gatsby-ssr.js
@@ -0,0 +1,49 @@
+import React, {Fragment} from 'react'
+import {withPrefix} from 'gatsby-link'
+import {defaultOptions} from './internals'
+
+// -------------------------------------------------------------
+// Module.
+// -------------------------------------------------------------
+
+function prefixWithSlash(val) {
+  if (val.charAt(0) !== '/') {
+    val = '/' + val
+  }
+
+  return val
+}
+
+// Base code from:
+// https://github.com/gatsbyjs/gatsby/blob/master/packages/gatsby-plugin-feed/src/gatsby-ssr.js
+
+exports.onRenderBody = ({setHeadComponents}, pluginOptions) => {
+  const {feeds} = {
+    ...defaultOptions,
+    ...pluginOptions
+  }
+
+  const links = feeds.map(({atomPath, jsonPath}, i) => {
+    atomPath = prefixWithSlash(atomPath)
+    jsonPath = prefixWithSlash(jsonPath)
+
+    return (
+      <Fragment>
+        <link
+          key={`gatsby-plugin-feeds-${i}-atom`}
+          rel="alternate"
+          type="application/atom+xml"
+          href={withPrefix(atomPath)}
+        />
+        <link
+          key={`gatsby-plugin-feeds-${i}-json`}
+          rel="alternate"
+          type="application/json"
+          href={withPrefix(jsonPath)}
+        />
+      </Fragment>
+    )
+  })
+
+  setHeadComponents(links)
+}
