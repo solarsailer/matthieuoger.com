7c4fca8864fc91dc142e6e7651b70fada3f652e1
Matthieu Oger <moger@pixelnest.io>
2018-06-06 15:15:45 +0200

    Extract Post component from PostTemplate

---
+++ src/components/Post.js
@@ -0,0 +1,81 @@
+import React from 'react'
+import styled from 'styled-components'
+import {rgba} from 'polished'
+
+import {colors} from '../styles/config'
+import postStyles from '../styles/components/post'
+
+// -------------------------------------------------------------
+// Components.
+// -------------------------------------------------------------
+
+const Wrapper = styled.div`
+  position: relative;
+  z-index: 1;
+`
+
+const Article = styled.article`
+  padding: 5rem;
+
+  background: white;
+  border: 1px solid rgba(0, 0, 0, 0.05);
+  border-radius: 3px;
+
+  &::before {
+    content: '';
+
+    display: block;
+
+    position: absolute;
+    z-index: -1;
+
+    top: 2rem;
+    bottom: 1rem;
+    left: 1rem;
+    right: 1rem;
+
+    box-shadow: 0 20px 30px ${rgba('black', 0.1)};
+  }
+`
+
+const Header = styled.header`
+  margin-bottom: 6rem;
+
+  text-align: center;
+  line-height: normal;
+`
+
+const Title = styled.h1`
+  margin-bottom: 0.5rem;
+  font-size: 2em;
+  font-weight: normal;
+`
+
+const Subtitle = styled.h2`
+  margin-bottom: 0;
+  color: #aaa;
+  font-size: 0.8em;
+  font-weight: normal;
+`
+
+const Content = styled.div`
+  ${postStyles};
+`
+
+// -------------------------------------------------------------
+// Default.
+// -------------------------------------------------------------
+
+export default ({title, date, content}) => {
+  return (
+    <Wrapper>
+      <Article>
+        <Header>
+          <Title>{title}</Title>
+          <Subtitle>{date}</Subtitle>
+        </Header>
+        <Content dangerouslySetInnerHTML={{__html: content}} />
+      </Article>
+    </Wrapper>
+  )
+}
--- src/components/templates/PostTemplate.js
+++ src/components/templates/PostTemplate.js
@@ -1,87 +1,31 @@
-import React from 'react'
+import React, {Fragment} from 'react'
 import styled from 'styled-components'
 import {Helmet} from 'react-helmet'
-import {rgba} from 'polished'
 
-import {colors} from '../../styles/config'
-import postStyles from '../../styles/components/post'
+import Post from '../Post'
 
 // -------------------------------------------------------------
-// Components.
+// Default.
 // -------------------------------------------------------------
-
-const Wrapper = styled.div`
-  position: relative;
-  z-index: 1;
-`
-
-const Article = styled.article`
-  padding: 5rem;
-
-  background: white;
-  border: 1px solid rgba(0, 0, 0, 0.05);
-  border-radius: 3px;
-
-  &::before {
-    content: '';
-
-    display: block;
-
-    position: absolute;
-    z-index: -1;
-
-    top: 2rem;
-    bottom: 1rem;
-    left: 1rem;
-    right: 1rem;
-
-    box-shadow: 0 20px 30px ${rgba('black', 0.1)};
-  }
-`
-
-const Titles = styled.div`
-  margin-bottom: 6rem;
-
-  text-align: center;
-  line-height: normal;
-`
-
-const MainTitle = styled.h1`
-  margin-bottom: 0.5rem;
-  font-size: 2em;
-  font-weight: normal;
-`
-
-const SubTitle = styled.h2`
-  margin-bottom: 0;
-  color: #aaa;
-  font-size: 0.8em;
-  font-weight: normal;
-`
-
-const PostContent = styled.div`
-  ${postStyles};
-`
 
 export default function PostTemplate({data}) {
-  const {markdownRemark, site} = data
-  const {frontmatter, html} = markdownRemark
+  const {
+    markdownRemark: {
+      frontmatter: {title, readableDate},
+      html
+    },
+    site: {siteMetadata}
+  } = data
 
   return (
-    <Wrapper>
-      <Article>
-        <Helmet>
-          <title>
-            {frontmatter.title} — {site.siteMetadata.title}
-          </title>
-        </Helmet>
-        <Titles>
-          <MainTitle>{frontmatter.title}</MainTitle>
-          <SubTitle>{frontmatter.date}</SubTitle>
-        </Titles>
-        <PostContent dangerouslySetInnerHTML={{__html: html}} />
-      </Article>
-    </Wrapper>
+    <Fragment>
+      <Helmet>
+        <title>
+          {title} — {siteMetadata.title}
+        </title>
+      </Helmet>
+      <Post title={title} date={readableDate} content={html} />
+    </Fragment>
   )
 }
 
@@ -100,7 +44,8 @@ export const POST_QUERY = graphql`
     markdownRemark(id: {eq: $id}) {
       html
       frontmatter {
-        date(formatString: "DD MMMM YYYY")
+        date
+        readableDate: date(formatString: "DD MMMM YYYY")
         title
       }
     }
