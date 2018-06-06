1cb821f2788e030b64da1433a9e4f0d195d778b6
Matthieu Oger <moger@pixelnest.io>
2018-06-06 12:29:35 +0200

    Add MiniPostCard component

---
+++ src/components/MiniPostCard.js
@@ -0,0 +1,129 @@
+import React from 'react'
+import styled from 'styled-components'
+
+import {colors} from '../styles/config'
+
+// -------------------------------------------------------------
+// Constants.
+// -------------------------------------------------------------
+
+const CARD_TRANSITION = 'all 0.2s ease-out'
+
+// -------------------------------------------------------------
+// Components.
+// -------------------------------------------------------------
+
+// We use a wrapper to create a shadow with a ::before pseudo-element.
+// This allows us to create a shadow smaller than the box of the actual element.
+// The shadow is more distant and subtle.
+
+const CardWrapper = styled.div`
+  position: relative;
+  z-index: 1;
+
+  transition: ${CARD_TRANSITION};
+
+  &:hover {
+    transform: translateY(-5px);
+  }
+`
+
+const Card = styled.article`
+  padding: 2rem;
+
+  background: white;
+  border: 1px solid rgba(0, 0, 0, 0.05);
+  border-radius: 3px;
+
+  transition: ${CARD_TRANSITION};
+
+  &::before {
+    content: '';
+
+    display: block;
+
+    position: absolute;
+    z-index: -1;
+
+    top: 2rem;
+    bottom: 1rem;
+    left: 1rem;
+    right: 1rem;
+
+    box-shadow: 0 20px 30px rgba(0, 0, 0, 0.1);
+
+    transition: ${CARD_TRANSITION};
+  }
+
+  &:hover {
+    color: white;
+    background: ${colors.brand.main};
+
+    &::before {
+      /* Increase the shadow height and force. */
+
+      left: 0;
+      right: 0;
+
+      box-shadow: 0 20px 40px rgba(0, 0, 0, 0.4);
+    }
+  }
+`
+
+const Header = styled.header`
+  line-height: normal;
+`
+
+const Content = styled.div`
+  margin-top: 2rem;
+
+  > :last-child {
+    margin-bottom: 0;
+  }
+`
+
+const Title = styled.h2`
+  margin-bottom: 0.5rem;
+
+  font-size: 0.9em;
+  font-weight: 500;
+`
+
+const Date = styled.p`
+  color: rgba(0, 0, 0, 0.5);
+
+  margin-bottom: 0;
+
+  font-size: 0.7em;
+  font-weight: 500;
+`
+
+const Excerpt = styled.p`
+  font-size: 0.8em;
+`
+
+// -------------------------------------------------------------
+// Export.
+// -------------------------------------------------------------
+
+export default ({frontmatter, excerpt}) => {
+  return (
+    <CardWrapper>
+      <Card>
+        <Header>
+          <Title>{frontmatter.title}</Title>
+          <Date>{frontmatter.date} </Date>
+        </Header>
+        {frontmatter.excerpt && (
+          <Content>
+            <Excerpt
+              dangerouslySetInnerHTML={{
+                __html: frontmatter.excerpt
+              }}
+            />
+          </Content>
+        )}
+      </Card>
+    </CardWrapper>
+  )
+}
