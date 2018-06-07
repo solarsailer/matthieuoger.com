13d4326a06921b6802934a30a62107f552b21831
Matthieu Oger <moger@pixelnest.io>
2018-06-07 19:52:44 +0200

    Create invisible link styles

---
+++ src/styles/components/invisible-link.js
@@ -0,0 +1,27 @@
+import {css} from 'styled-components'
+import {tint, shade} from 'polished'
+
+import {colors} from '../config'
+
+// -------------------------------------------------------------
+// Invisible Link.
+//
+// For links that are non-important (e.g., an anchor).
+// -------------------------------------------------------------
+
+export default css`
+  a {
+    color: inherit;
+    text-decoration: none;
+    transition: color 0.2s ease-in;
+  }
+
+  a:hover {
+    color: ${colors.brand.main};
+  }
+
+  a:focus,
+  a:active {
+    color: ${shade(0.9, colors.brand.main)};
+  }
+`
