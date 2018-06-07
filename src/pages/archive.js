07caadf85985a11d7155ca08c1b55b969060463b
Matthieu Oger <moger@pixelnest.io>
2018-06-07 12:02:45 +0200

    Add headers to archive list

--- src/pages/archive.js
+++ src/pages/archive.js
@@ -3,6 +3,62 @@ import styled from 'styled-components'
 import Link from 'gatsby-link'
 
 import PostRecap from '../components/PostRecap'
+import HeaderRow from '../components/HeaderRow'
+
+// -------------------------------------------------------------
+// Functions.
+// -------------------------------------------------------------
+
+function convertEdgeToNode(x) {
+  return x.node
+}
+
+function getYear(node) {
+  return new Date(node.frontmatter.date).getUTCFullYear()
+}
+
+function createRow(node) {
+  return (
+    <li key={node.id}>
+      <PostRecap
+        url={node.fields.path}
+        title={node.frontmatter.title}
+        date={node.frontmatter.readableDate}
+      />
+    </li>
+  )
+}
+
+function createHeaderRow(year) {
+  return (
+    <li key={`archive-header-row-${year}`}>
+      <HeaderRow>{year}</HeaderRow>
+    </li>
+  )
+}
+
+function groupByYear(items) {
+  return items.reduce((acc, x, i) => {
+    const year = getYear(x)
+
+    // If first row, just add the header directly.
+    if (i === 0) {
+      acc.push(createHeaderRow(year))
+    }
+    // Otherwise, check if the current year is different from the previous node.
+    else {
+      const previousItem = items[i - 1]
+
+      // Different? We have changed the year and we should add the header.
+      if (getYear(previousItem) !== year) {
+        acc.push(createHeaderRow(year))
+      }
+    }
+
+    acc.push(createRow(x))
+    return acc
+  }, [])
+}
 
 // -------------------------------------------------------------
 // Component.
@@ -17,17 +73,7 @@ export default ({data}) => {
 
   return (
     <div>
-      <List>
-        {edges.map(({node}) => (
-          <li key={node.id}>
-            <PostRecap
-              url={node.fields.path}
-              title={node.frontmatter.title}
-              date={node.frontmatter.readableDate}
-            />
-          </li>
-        ))}
-      </List>
+      <List>{groupByYear(edges.map(convertEdgeToNode))}</List>
     </div>
   )
 }
