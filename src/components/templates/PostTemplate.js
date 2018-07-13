29d582d87a1ef3a5504eb6a2bef3a979095f8309
Matthieu Oger <moger@pixelnest.io>
2018-07-13 19:12:00 +0200

    Change metadata for each post

--- src/components/templates/PostTemplate.js
+++ src/components/templates/PostTemplate.js
@@ -1,9 +1,24 @@
 import React, {Fragment} from 'react'
 import styled from 'styled-components'
 import {Helmet} from 'react-helmet'
+import {URL} from 'url'
 
 import PageTitle from '../PageTitle'
 import Post from '../Post'
+
+// -------------------------------------------------------------
+// Functions.
+// -------------------------------------------------------------
+
+function getExcerpt(excerptLine, excerptQuote) {
+  if (excerptLine) return excerptLine
+
+  if (excerptQuote) {
+    return `${excerptQuote.name}: “${excerptQuote.quote}”`
+  }
+
+  return null
+}
 
 // -------------------------------------------------------------
 // Default.
@@ -12,12 +27,24 @@ import Post from '../Post'
 export default function PostTemplate({data}) {
   const {
     markdownRemark: {
-      frontmatter: {title, readableDate, showPageTitle},
+      frontmatter: {
+        title,
+        readableDate,
+        showPageTitle,
+        excerptLine,
+        excerptQuote
+      },
       fields: {path},
       html
     },
     site: {siteMetadata}
   } = data
+
+  const IsomorphicURL = URL ? URL : window.URL
+
+  const postUrl = new IsomorphicURL(path, siteMetadata.siteUrl).toString()
+  const postDescription =
+    getExcerpt(excerptLine, excerptQuote) || siteMetadata.description
 
   return (
     <Fragment>
@@ -25,6 +52,19 @@ export default function PostTemplate({data}) {
         <title>
           {title} — {siteMetadata.author}
         </title>
+
+        <meta name="twitter:title" content={title} />
+        <meta property="og:title" content={title} />
+
+        <meta name="twitter:url" content={postUrl} />
+        <meta property="og:url" content={postUrl} />
+
+        {postDescription && (
+          <meta name="twitter:description" content={postDescription} />
+        )}
+        {postDescription && (
+          <meta property="og:description" content={postDescription} />
+        )}
       </Helmet>
       {showPageTitle ? (
         <Fragment>
@@ -47,7 +87,9 @@ export const POST_QUERY = graphql`
     site {
       siteMetadata {
         title
+        description
         author
+        siteUrl
       }
     }
 
@@ -58,6 +100,11 @@ export const POST_QUERY = graphql`
         readableDate: date(formatString: "DD MMMM YYYY")
         title
         showPageTitle
+        excerptLine: excerpt
+        excerptQuote {
+          name
+          quote
+        }
       }
       fields {
         path
