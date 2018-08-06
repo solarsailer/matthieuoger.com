6978b5f299b207175d958b3826602cb2af91800d
Matthieu Oger <moger@pixelnest.io>
2018-08-06 18:17:19 +0200

    Use built-in Gatsby Link component

--- src/components/layouts/Header.js
+++ src/components/layouts/Header.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import Link from 'gatsby-link'
+import {Link} from 'gatsby'
 import posed from 'react-pose'
 import {random} from 'lodash'
 import styled, {keyframes} from 'styled-components'
--- src/pages/404.js
+++ src/pages/404.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import Helmet from 'react-helmet'
 import styled from 'styled-components'
-import Link from 'gatsby-link'
+import {Link} from 'gatsby'
 
 import {Button} from '../components/Button'
 
