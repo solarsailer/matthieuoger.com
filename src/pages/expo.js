f2c350e197f22a8f487268be33586418fabe0727
Matthieu Oger <moger@pixelnest.io>
2023-04-29 10:26:09 +0200

    Update expo

--- src/pages/expo.js
+++ src/pages/expo.js
@@ -16,20 +16,20 @@ const md = `
   <p>J'organise une <strong>exposition petit format de mes photographies</strong>. Le thème est centré autour de Paris, ses gens, et ses rues.</p>
 
   <p>
-    Le vernissage aura lieu le <strong>8 Juin 2023 à 19h30 au Tago Mago</strong>, dans le 10ème arrondissement de Paris.
+    Le vernissage aura lieu le <strong>8 Juin 2023 à 19h00 au Tago Mago</strong>, dans le 10ème arrondissement de Paris.
   </p>
 
   <p>N'hésitez pas à passer prendre un verre et regarder les quelques photos.</p>
 
   <p><iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d5249.289839194645!2d2.354427148698112!3d48.86498104315739!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47e66e0e82207263%3A0x39676a858a43f71!2sTago%20Mago!5e0!3m2!1sen!2sfr!4v1682629573227!5m2!1sen!2sfr" width="1000" height="300" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe></p>
 
-  <p><strong>L'exposition se prolongera jusqu'au 14 Juin.</strong> Les œuvres seront à vendre, n'hésitez pas à voir avec le gérant du bar si l'une d'entre elles vous intéresse.</p>
+  <p><strong>L'exposition se prolongera jusqu'au 14 Juin.</strong> Les "œuvres" seront à vendre, n'hésitez pas à voir avec le gérant du bar si l'une d'entre elles vous intéresse.</p>
 
   <p>A bientôt au vernissage !</p>
 
   <hr />
 
-  <img src="/images/temp/flyer.jpg" style="width: 100%; max-width: 850px;" />
+  <img src="/images/temp/expo-flyer.jpg" style="width: 100%; max-width: 850px;" />
 `
 
 const metaTitle = 'Exposition Photos, Tago Mago, Paris 10 — Matthieu Oger'
@@ -51,8 +51,10 @@ export default () => {
         <meta property="og:url" content={metaUrl} />
         <meta name="twitter:description" content={metaDescription} />
         <meta property="og:description" content={metaDescription} />
+        <meta name="twitter:image" content="/images/temp/expo-social.jpg" />
+        <meta property="og:image" content="/images/temp/expo-social.jpg" />
       </Helmet>
-      <PageTitle>Exposition Photos</PageTitle>
+      <PageTitle>Expo Photos</PageTitle>
       <Post
         url="/expo/"
         date="08 Juin 2023"
